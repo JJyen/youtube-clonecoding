a8883ac53fa6e4fbd32805f1b25baff8490def5f
JJyen <gdb1632@naver.com>
2025-06-21 20:26:12 +0900

    2.3 프로젝트 초기 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="font-bold text-pink-500">hello world</p>
+      <Button variant={"new"}>Click me!</Button>
+    </div>
+  );
+}
